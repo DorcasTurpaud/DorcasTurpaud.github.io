519dae678d2f7555cecaefa52e7dbd8dbecbc130
JazzyLazzy <37636333+JazzyLazzy@users.noreply.github.com>
2019-10-22 07:23:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function heading(){
     var h = window.innerHeight;
-    document.getElementById("heading").style.height = h-10 + "px";
+    document.getElementById("heading").style.height = h-.01h + "px";
+    document.getElementById("hmbc").style.heigh = h-.01h + "px";
     console.log(h);
 }
