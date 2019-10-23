185e5c28a735662b91a3d9269995d35eebd9adaa
DorcasTurpaud <56858610+DorcasTurpaud@users.noreply.github.com>
2019-10-22 21:53:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function heading(){
     var h = window.innerHeight;
     document.getElementById("heading").style.height = h-.01*h + "px";
-    document.getElementById("charged").style.heigh = h + "px";
+    document.getElementById("charged").style.height = h + "px";
     console.log(h);
 }
